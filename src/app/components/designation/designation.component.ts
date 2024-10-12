e6e4002a8cc72d8bd4d2c6434869f68c6610d485
Anusha S Puranikmath <anusha.puranikmath47@gmail.com>
2024-10-12 13:12:41 +0530

    Added Loader in designation component

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -13,6 +13,7 @@ import { HttpClient } from '@angular/common/http';
 export class DesignationComponent implements OnInit {
 
   designationList: IDesignation[] = [];
+  isLoader: boolean = true;
   masterService = inject(MasterService);
   http = inject(HttpClient);
 
@@ -23,8 +24,10 @@ export class DesignationComponent implements OnInit {
   getAllDesignation() {
     this.masterService.getDesignations().subscribe((res: APIResponseModel) => {
       this.designationList = res.data;
+      this.isLoader = false;
     }, error => {
       alert("API Error: "+ error);
+      this.isLoader = false; 
     });
   }
 }
