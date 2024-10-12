0228b66902131c6355beb21c6a3f9a88a734bee4
Anusha S Puranikmath <anusha.puranikmath47@gmail.com>
2024-10-12 11:33:52 +0530

    added table to show roles

--- src/app/model/interface/role.ts
+++ src/app/model/interface/role.ts
@@ -2,3 +2,14 @@ export interface IRole {
     roleId: number,
     role: string
 }
+
+export interface APIResponseModel {
+    message: string,
+    data: any,
+    resule: boolean
+}
+
+export interface IDesignation {
+    designationId: number,
+    designation: string
+}
